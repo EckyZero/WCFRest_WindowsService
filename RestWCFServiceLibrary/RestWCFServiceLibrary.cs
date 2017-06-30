6fc92d09965c02be30652fa94ebd44275be58488
Erik Andersen <erik.andersen@parivedasolutions.com>
2017-06-30 12:44:18 -0600

    Initial Commit

---
+++ RestWCFServiceLibrary/RestWCFServiceLibrary.cs
@@ -0,0 +1,20 @@
+﻿namespace RestWCFServiceLibrary
+{
+    public class RestWCFServiceLibrary : IRestWCFServiceLibrary
+    {
+        public string XMLData(string id)
+        {
+            return Data(id);
+        }
+        public string JSONData(string id)
+        {
+            return Data(id);
+        }
+
+        private string Data(string id)
+        {
+            // logic
+            return "Data: " + id;
+        }
+    }
+}
---
+++ WindowsService1/MyRestWCFRestWinSerInstaller.cs
@@ -0,0 +1,35 @@
+﻿namespace RestWCFWinService
+{
+    #region Namespaces
+    using System.ComponentModel;
+    using System.ServiceProcess;
+    #endregion
+
+    [RunInstaller(true)]
+    public partial class MyRestWCFRestWinSerInstaller : System.Configuration.Install.Installer
+    {
+        private ServiceProcessInstaller process;
+        private ServiceInstaller service;
+
+        public MyRestWCFRestWinSerInstaller()
+        {
+            process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+            service = new ServiceInstaller();
+            service.ServiceName = "Demo";
+            service.Description = "WCF REST API Hosting on Windows Service";
+            service.DelayedAutoStart = true;
+            Installers.Add(process);
+            Installers.Add(service);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
