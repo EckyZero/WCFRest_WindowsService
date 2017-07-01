128030532dde5aa6504d7a82f97c49e9e0d43c37
Erik Andersen <erik.andersen@parivedasolutions.com>
2017-06-30 21:55:35 -0600

    - Adding SQLite support

--- RestWCFServiceLibrary/RestWCFServiceLibrary.cs
+++ RestWCFServiceLibrary/RestWCFServiceLibrary.cs
@@ -1,4 +1,7 @@
-﻿namespace RestWCFServiceLibrary
+﻿using System;
+using System.Data.SQLite;
+
+namespace RestWCFServiceLibrary
 {
     public class RestWCFServiceLibrary : IRestWCFServiceLibrary
     {
@@ -13,8 +16,47 @@
 
         private string Data(string id)
         {
-            // logic
-            return "Data: " + id;
+            SQLiteConnection m_dbConnection;
+            string sql;
+            SQLiteCommand command;
+
+            if (!System.IO.File.Exists("MyDatabase.sqlite"))
+            {
+                SQLiteConnection.CreateFile("MyDatabase.sqlite");
+
+                m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+                m_dbConnection.Open();
+
+                sql = "create table highscores (name varchar(20), score int)";
+
+                command = new SQLiteCommand(sql, m_dbConnection);
+                command.ExecuteNonQuery();
+
+                sql = "insert into highscores (name, score) values ('Me', 9001)";
+                command = new SQLiteCommand(sql, m_dbConnection);
+                command.ExecuteNonQuery();
+
+            }
+            else
+            {
+                m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+                m_dbConnection.Open();
+            }
+
+            sql = "select * from highscores";
+            command = new SQLiteCommand(sql, m_dbConnection);
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var name = reader["name"];
+                var score = reader["score"];
+                Console.WriteLine("Name: " + name + "\tScore: " + score);
+            }
+                
+
+            return "HWLLO";
         }
     }
 }
